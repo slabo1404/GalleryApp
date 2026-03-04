71d9b3c1f8991ff68e84cd1a7a6f27959853af55
Slava <slabo1404@gmailcom>
2026-03-04 14:15:39 +0300

    Initial commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Вячеслав Болбат on 4.03.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
